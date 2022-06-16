f159c0380a6164bf289313bab9aa306e587acf2d
Guembe Blessing <blessingodede@gmail.com>
2022-06-16 04:16:42 +0100

    Update settings.py

--- moziotest/settings.py
+++ moziotest/settings.py
@@ -9,24 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
-import sys
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-# Hack for running app directly.
-sys.path.insert(0, os.path.abspath('..'))
-# Hack for tests. 
-sys.path.insert(0, os.path.abspath('.'))
-
-
-
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -134,5 +121,5 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-import django_heroku
-django_heroku.settings(locals())
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
